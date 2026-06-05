716cb5c5dfd0b37db410d9242a29092bb1f8890f
LeeVHoangtk3 <leviethoangtk3@gmail.com>
2026-06-06 00:25:38 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,27 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-const originalFetch = window.fetch;
-window.fetch = (input, init) => {
-  if (typeof input === 'string' && input.startsWith('/')) {
-    const baseUrl = process.env.REACT_APP_API_URL || '';
-    const newInit = { ...init };
-    
-    newInit.credentials = 'include';
-    return originalFetch(baseUrl + input, newInit);
-  }
-  return originalFetch(input, init);
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-
 
 reportWebVitals();
